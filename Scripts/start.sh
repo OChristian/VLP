fe90b2ed69b2dabaedd8f3a478a76715e6ac3ab9
OChristian <christian.oberle@ca.com>
2026-02-11 04:19:15 -0500

    Update start.sh

--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-k_pod=$(kubectl get pods | grep ssg)
-pod="${k_pod%% *}"
-echo "Pod name: ${pod}"
+# Restarting the main gateway pod in default namespace
+k_pod=$(microk8s kubectl get pods | grep ssg)
+ssg_pod="${k_pod%% *}"
+microk8s kubectl delete pod ${ssg_pod}
+echo "Restarting gateway ssg in default namespace: ${ssg_pod}"
+microk8s get pods 
+
+# Restarting the otk pod in otk namespace
+k_pod=$(microk8s kubectl get pods -n otk | grep ssg)
+otk_pod="${k_pod%% *}"
+microk8s kubectl delete pod ${otk_pod} -n otk
+
+echo "Restarting gateway ssg in otk namespace: ${otk_pod}"
+microk8s kubectl get pods -n otk
